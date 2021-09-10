3654af89d398f0c80a68d2fc0f6724478fcb5364
Edson Matheus <edsonmaatheus@gmail.com>
2021-09-10 18:07:06 -0300

    Home e navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,24 @@
+import Footer from "components/footer";
+import NavBar from "components/navbar";
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
-        <div >
-            Página Home
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                </div>
+                <Link className="btn btn-primary btn-ng" to="/dashboard">
+                    Acessar o Dashboard
+                </Link>
+            </div>
+            <Footer />
+        </>
     );
 }
-
 export default Home;
